ad859be3f5639735f6d7d33f415a21468137b496
ayoubleass <ayoublea@gmail.com>
2024-11-21 12:44:42 +0000

    CREATE file : 0-rotate_2d_matrix.py && main_0.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -112,4 +112,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             raise ValueError
-        self._db.update_user(user.id, password=password, reset_token=None)
+        self._db.update_user(
+                user.id, hashed_password=_hash_password(password),
+                reset_token=None)
