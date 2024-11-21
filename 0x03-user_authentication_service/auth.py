db698369c921a983b761cc4afd57852ff71e6b12
ayoubleass <ayoublea@gmail.com>
2024-11-21 12:41:23 +0000

    update app.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -112,4 +112,4 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             raise ValueError
-        self._db.update_user(password=password, reset_token=None)
+        self._db.update_user(user.id, password=password, reset_token=None)
